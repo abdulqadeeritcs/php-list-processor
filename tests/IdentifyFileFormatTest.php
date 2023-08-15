51bb03cca63719da6f54893d4ec9888591c8e078
AbdulQ <abdulqadeerit@gmail.com>
2023-08-15 22:29:54 +0100

    Add Identify File Format Test

---
+++ tests/IdentifyFileFormatTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once './scriptc.php';
+$te = new FileProcessor();
+
+class IdentifyFileFormatTest extends TestCase
+{
+
+    public function testIdentifyFileFormatWithExtension()
+    {
+        global $te;
+        $filename = 'products_1.csv';
+        $format = $te->identifyFileFormat($filename);
+        $this->assertEquals('csv', $format);
+    }
+
+    public function testIdentifyFileFormatWithoutExtension()
+    {
+        global $te;
+        $filename = 'products_1no_extension';
+        $format = $te->identifyFileFormat($filename);
+        $this->assertNull($format);
+    }
+
+    public function testIdentifyFileFormatWithUppercaseExtension()
+    {
+        global $te;
+        $filename = 'products_1.CSV';
+        $format = $te->identifyFileFormat($filename);
+        $this->assertEquals('csv', $format);
+    }
+
+    public function testIdentifyFileFormatWithMultipleDots()
+    {
+        global $te;
+        $filename = 'products_1.tar.gz';
+        $format = $te->identifyFileFormat($filename);
+        $this->assertEquals('gz', $format);
+    }
+
+    public function testIdentifyFileFormatWithNoFilename()
+    {
+        global $te;
+        $format = $te->identifyFileFormat();
+        $this->assertNull($format);
+    }
+}
+?>
